88d3b4634dc83e546ead665b129f140e4ea575cf
Sarju Poudel <sarju.poudel@gmail.com>
2026-02-26 18:41:30 -0600

    Updated the lecture 12

--- Lecture/Lecture12/BissectionMain.c
+++ Lecture/Lecture12/BissectionMain.c
@@ -3,8 +3,8 @@ submit both source codes and screenshots*/
 #include "BissectionMethod.h"
 
 int main() {
-    double a = 1.0; // Starting point a
-    double b = 2.0; // Starting point b
+    double a = 1.0; // left bound
+    double b = 2.0; // right bound
     double tol = 0.000001; // Tolerance
     int maxIter = 100; // Maximum iterations
 
