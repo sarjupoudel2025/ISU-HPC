9857aac07193f93b48b0668d242e151e28f3b289
Sarju Poudel <sarju.poudel@gmail.com>
2026-05-05 11:43:21 -0500

    updated the bash script

--- Project/GMRES_mpi/Run_mpi.sh
+++ Project/GMRES_mpi/Run_mpi.sh
@@ -4,9 +4,9 @@
 #SBATCH --ntasks-per-node=32
 #SBATCH --time=0-0:30:0
 #SBATCH --qos=instruction
-#SBATCH --job-name="heat2d_mpi"
-#SBATCH --output=".out"
-#SBATCH --error="errors_heat2d_mpi.out"
+#SBATCH --job-name="2D_Fluid_MPI_GMRES"
+#SBATCH --output="log.out"
+#SBATCH --error="error.out"
 #SBATCH --partition=instruction
 
 # -------------------------------
